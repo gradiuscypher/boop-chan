f72faa7e228c413423f7b8d960d7c6091e574631
gradiuscypher <gradiuscypher@gmail.com>
2025-09-01 17:27:34 +0000

    making some baseline models

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fastapi-sqla-shadcn
-A template project using FastAPI, SQLAlchemy, Vite, and Shadcn for quick Webstack stuff
+# boop-chan
+A Discord banpool manager and bot
---
+++ api/models/banpool.py
@@ -0,0 +1,58 @@
+import datetime
+from enum import IntEnum
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy import Column, Integer, String, select, DateTime
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import Base
+
+
+class AdminAudit(BaseModel):
+    id: int
+    action: str
+    created_at: datetime.datetime
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BanActionAudit(BaseModel):
+    id: int
+    ban_id: int
+    action: str
+    created_at: datetime.datetime
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Ban(BaseModel):
+    id: int
+    target_discord_id: int
+    target_user_name: str
+    target_user_display_name: str
+    author_discord_id: int
+    author_discord_name: str
+    reason: str
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+    expires_at: datetime.datetime
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Banpool(BaseModel):
+    id: int
+    name: str
+    description: str
+    model_config = ConfigDict(from_attributes=True)
+    bans: list[Ban]
+
+class BanpoolSubscriptionLevel(IntEnum):
+    ALERT = 1
+    BAN = 2
+
+class BanpoolSubscription(BaseModel):
+    id: int
+    banpool_id: int
+    created_at: datetime.datetime
+    author_discord_id: int
+    author_discord_name: str
+    guild_id: int
+    guild_name: str
+    model_config = ConfigDict(from_attributes=True)
